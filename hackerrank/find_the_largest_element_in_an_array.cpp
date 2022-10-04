0ac1a3e64648d45edb5617f6184351c5a6f579bc
kahuku <buzzinbubba@gmail.com>
2022-10-04 12:32:59 -0600

    another emergent hackerrank solve

---
+++ hackerrank/find_the_largest_element_in_an_array.cpp
@@ -0,0 +1,43 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int a_size;
+    int prev;
+    int curr;
+    
+    cin >> a_size;
+    cin >> prev;
+    
+    for (int i = 0; i < a_size - 1; i++) {
+        cin >> curr;
+        if (curr < prev) {
+            cout << i << endl;
+            return 0;
+        }
+    }
+    cout << 0 << endl;
+    return 0;
+}
