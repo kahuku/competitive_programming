cab5e64d334870afe87ad46f46e4bd949fe7b8f8
kahuku <buzzinbubba@gmail.com>
2023-02-19 21:50:30 -0700

    393 2/19

--- leetcode/maximum-product-subarray.py
+++ leetcode/maximum-product-subarray.py
@@ -10,3 +10,12 @@ class Solution:
 
             res = max(res, ma)
         return res
+    
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        curr_max, curr_min = nums[0], nums[0]
+        final_max = nums[0]
+        for i in range(1, len(nums)):
+            curr_max, curr_min = max(nums[i], nums[i] * curr_max, nums[i] * curr_min), min(nums[i], nums[i] * curr_max, nums[i] * curr_min)
+            final_max = max(final_max, curr_max)
+        return final_max
