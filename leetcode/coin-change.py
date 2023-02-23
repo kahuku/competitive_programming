33b1e29d7324f0758d04cb0d7dfb37d3e3b309a4
kahuku <buzzinbubba@gmail.com>
2023-02-23 11:48:58 -0700

    393 2/23

--- leetcode/coin-change.py
+++ leetcode/coin-change.py
@@ -10,3 +10,15 @@ class Solution:
             return min([1 + getCoins(amount - coin) for coin in coins])
         numCoins = getCoins(amount)
         return numCoins if numCoins != float('inf') else -1
+    
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        coins.sort()
+        dp = [0]
+        for i in range(1, amount + 1):
+            dp.append(float('inf'))
+            for coin in coins:
+                if i - coin < 0:
+                    break
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+        return dp[-1] if dp[-1] != float('inf') else -1
