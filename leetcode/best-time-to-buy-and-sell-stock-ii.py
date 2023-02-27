87af0f33a8735351fdc3988f489d3731c210ce08
kahuku <buzzinbubba@gmail.com>
2023-02-27 12:38:48 -0700

    393 2/27

--- leetcode/best-time-to-buy-and-sell-stock-ii.py
+++ leetcode/best-time-to-buy-and-sell-stock-ii.py
@@ -7,3 +7,22 @@ class Solution:
                 s += r - l
             l = r
         return s
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        for i in range(1, len(prices)):
+            profit += prices[i] - prices[i - 1] if prices[i] > prices[i - 1] else 0
+        return profit
+    
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        return sum([prices[i] - prices[i - 1] for i in range(1, len(prices)) if prices[i] > prices[i - 1]])
+    
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        return sum([max(b-a, 0) for a, b in zip(prices, prices[1:])])
+    
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        return sum(max(b-a, 0) for a, b in zip(prices, prices[1:]))
