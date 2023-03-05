8307302266844a285c9b1ee4aea22ecd87cdf94a
kahuku <buzzinbubba@gmail.com>
2023-03-04 18:56:11 -0700

    393 3/4

--- leetcode/maximum-subarray.py
+++ leetcode/maximum-subarray.py
@@ -4,3 +4,10 @@ class Solution:
         for i, num in enumerate(nums):
             dp.append(max(dp[i] + num, num))
         return max(dp)
+    
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        dp = nums.copy()
+        for i in range(1, len(nums)):
+            dp[i] = max(dp[i], dp[i - 1] + dp[i])
+        return max(dp)
