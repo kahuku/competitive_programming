192e6b413dece159b694ffe356af12a97caa0159
kahuku <buzzinbubba@gmail.com>
2023-09-13 11:05:46 -0600

    cpc 9/13

---
+++ kattis/sith.py
@@ -0,0 +1,9 @@
+force_user = input()
+a, b, c = int(input()), int(input()), int(input())
+if a - b >= 0:
+    print("VEIT EKKI")
+else:
+    if c > 0:
+        print("SITH")
+    else:
+        print("JEDI")
