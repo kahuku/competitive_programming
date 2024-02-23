ad88c58648833b2da1e6d27a1b2c52f73f90309b
kahuku <buzzinbubba@gmail.com>
2024-02-23 15:36:28 -0700

    int as sum of pows

---
+++ 393-interviews/DP/Integer as Sum of Powers/ways-to-express-an-integer-as-sum-of-powers.py
@@ -0,0 +1,11 @@
+def numberOfWays(n: int, x: int) -> int:
+    dp = [1] + [0] * n
+    a = 1
+    mod = 10**9 + 7
+    while (v := pow(a, x)) <= n:
+        for i in range(n, v - 1, -1):
+            dp[i] = (dp[i] + dp[i - v]) % mod
+        a += 1
+    return dp[n]
+
+print(numberOfWays(int(input()), 2))
