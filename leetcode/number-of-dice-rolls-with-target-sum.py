19117b0a1d0feef4c81e0173b85ffed0955dbc2a
kahuku <buzzinbubba@gmail.com>
2024-03-18 12:17:17 -0600

    dice rolls

--- leetcode/number-of-dice-rolls-with-target-sum.py
+++ leetcode/number-of-dice-rolls-with-target-sum.py
@@ -31,3 +31,25 @@ class Solution:
 
         ways = search(n, target)
         return ways % (10 ** 9 + 7)
+    
+class Solution(object):
+    def numRollsToTarget(self, n, k, target):
+        dp = []
+        constant = 10**9 + 7
+        for i in range(n + 1):
+            dp.append([0] * (target + 1))
+        for j in range(target + 1):
+            if j < k:
+                dp[0][j] = 1
+        
+        for i in range(1, n + 1):
+            for j in range(1, target + 1):
+                res = 0
+                for z in range(1, k + 1):
+                    if j - z < 0:
+                        continue
+                    res += dp[i - 1][j - z]
+                dp[i][j] = res % constant
+        return dp[n - 1][target - 1]
+    
+print(Solution().numRollsToTarget(3, 6, 7))
