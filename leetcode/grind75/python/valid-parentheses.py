8f13cc41991e6145e2f584221137ae009752720d
kahuku <buzzinbubba@gmail.com>
2024-03-21 17:24:42 -0600

    grind75

---
+++ leetcode/grind75/python/valid-parentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        opens = '{(['
+        closes = '})]'
+        stack = []
+        for c in s:
+            if c in opens:
+                stack.append(c)
+            else:
+                i = closes.index(c)
+                if len(stack) == 0 or stack[-1] != opens[i]: return False
+                stack.pop()
+        return len(stack) == 0
