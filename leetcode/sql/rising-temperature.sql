9585ac1a58647717db2bdee7967076af103ac577
kahuku <buzzinbubba@gmail.com>
2024-06-08 17:32:36 -0600

    leetcode sql

--- leetcode/sql/rising-temperature.sql
+++ leetcode/sql/rising-temperature.sql
@@ -3,3 +3,9 @@ SELECT w1.id AS Id
 FROM Weather w1, Weather w2
 WHERE datediff(w1.recordDate, w2.recordDate) = 1
     AND w1.temperature > w2.temperature;
+
+# Write your MySQL query statement below
+SELECT w1.id AS Id
+FROM Weather w1 JOIN Weather w2
+WHERE datediff(w1.recordDate, w2.recordDate) = 1
+    AND w1.temperature > w2.temperature;
