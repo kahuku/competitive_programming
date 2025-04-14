ac82125782bf054d0c7b981a98030d2015a98178
kahuku <buzzinbubba@gmail.com>
2025-04-14 17:01:31 -0600

    design twitter

---
+++ leetcode/design-twitter.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+class Twitter:
+
+    def __init__(self):
+        self.following = defaultdict(set)
+        self.tweets = []
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweets.append((tweetId, userId))
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        # feed = []
+        # for i in range(len(self.tweets) - 1, -1, -1):
+        #     if self.tweets[i][1] in self.following[userId] or self.tweets[i][1] == userId:
+        #         feed.append(self.tweets[i][0])
+        # return feed[:10]
+        return [tweetId for tweetId, tweeterId in self.tweets[::-1] if tweeterId == userId or tweeterId in self.following[userId]][:10]
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.following[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if self.following[followerId] and followeeId in self.following[followerId]:
+            self.following[followerId].remove(followeeId)
+
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)
